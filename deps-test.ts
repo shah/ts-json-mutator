196df1f74cf18e40055a8b94abddbf8a08b03c24
Shahid N. Shah <shahid@shah.org>
2020-12-11 11:26:56 -0500

    fix: update dependency for Deno 1.6 upgrade

--- deps-test.ts
+++ deps-test.ts
@@ -1,3 +1,3 @@
 // Dependencies required during engineering/testing only (not in production)
-export * as testingAsserts from "https://deno.land/std@0.78.0/testing/asserts.ts";
-export * as path from "https://deno.land/std@0.78.0/path/mod.ts";
+export * as testingAsserts from "https://deno.land/std@0.80.0/testing/asserts.ts";
+export * as path from "https://deno.land/std@0.80.0/path/mod.ts";
